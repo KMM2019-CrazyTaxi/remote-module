bd87c739cc8962ebbccc3f35361e6a3010bb26e6
Henrik Nilsson <henni142@student.liu.se>
2019-11-18 13:49:10 +0100

    Add class for polling RemoteData at fixed timesteps. The FixedTimePoller is Runnable an is meant to be started on a thread separate from the main thread.

---
+++ src/remote/FixedTimePoller.java
@@ -0,0 +1,75 @@
+package remote;
+
+import exceptions.MissingIDException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedTimePoller implements Runnable {
+
+    private final Server server;
+    private volatile boolean running = false;
+    private int sleepDuration;
+    private List<RemoteData> dataPollers;
+
+    public FixedTimePoller(Server server, int sleepDuration) {
+        this.server = server;
+        this.sleepDuration = sleepDuration;
+        this.dataPollers = new ArrayList<>();
+    }
+
+    synchronized public void add(RemoteData o) {
+        dataPollers.add(o);
+    }
+
+    synchronized public void stop() {
+        running = false;
+    }
+
+    synchronized public int getSleepDuration() {
+        return sleepDuration;
+    }
+
+    synchronized public void setSleepDuration(int sleepDuration) {
+        this.sleepDuration = sleepDuration;
+    }
+
+    /**
+     * When an object implementing interface <code>Runnable</code> is used
+     * to create a thread, starting the thread causes the object's
+     * <code>run</code> method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method <code>run</code> is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    synchronized public void run() {
+        running = true;
+
+        while(running) {
+            // Add poll requests
+            for (RemoteData d : dataPollers){
+                d.poll();
+            }
+
+            // Pull
+            try {
+                this.server.pull();
+            } catch (IOException | MissingIDException e) {
+                e.printStackTrace();
+            }
+
+            // Sleep
+            // TODO add more robust threading behavoiur
+            try {
+                Thread.sleep(sleepDuration);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
