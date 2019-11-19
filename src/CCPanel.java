6df0864b584f30351c51a3d50500cc925d909bce
Henrik Nilsson <henni142@student.liu.se>
2019-11-19 19:25:42 +0100

    Add JavaFX GUI base with WIP empty tabs.

---
+++ src/CCPanel.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+
+public class CCPanel extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        Stage root = FXMLLoader.load(getClass().getResource("gui/fxml/UI.fxml"));
+        root.show();
+    }
+}
