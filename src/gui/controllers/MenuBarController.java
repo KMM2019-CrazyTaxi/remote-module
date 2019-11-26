a418b528e093cd3bd9565a5442ca4d37c3a21cac
Henrik Nilsson <henni142@student.liu.se>
2019-11-26 11:52:45 +0100

    Add JavaDoc to MenuBarController class and update imports. Change .menu.alive style to have a green background instead of just an inner shadow.

--- src/gui/controllers/MenuBarController.java
+++ src/gui/controllers/MenuBarController.java
@@ -1,23 +1,32 @@
 package gui.controllers;
 
-import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
+
 import remote.Server;
 
 import java.io.IOException;
 
+/**
+ * Menu Bar Controller class. This is the main controller of the Menu Bar.
+ */
 public class MenuBarController {
 
     @FXML private Menu serverMenu;
 
+    /**
+     * Handle click in the Server Connect-button. Tries to connect the server.
+     * @param actionEvent Triggering event
+     */
     public void handleConnectClick(ActionEvent actionEvent) {
         try {
             Server.getInstance().connect();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        // TODO Change alive status to a RemoteData of Car then implemented
         serverMenu.getStyleClass().add("alive");
     }
 }
