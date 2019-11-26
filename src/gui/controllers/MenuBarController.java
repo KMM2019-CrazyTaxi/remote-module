e294bac9fb31c6f58d1b9cf906875a2dd31f9d76
Henrik Nilsson <henni142@student.liu.se>
2019-11-26 10:44:44 +0100

    Update darker cc color to darken further. Update menu styling to add server alive status capability to the Server button. Add menu shadow on hover.

--- src/gui/controllers/MenuBarController.java
+++ src/gui/controllers/MenuBarController.java
@@ -1,11 +1,16 @@
 package gui.controllers;
 
+import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Menu;
 import remote.Server;
 
 import java.io.IOException;
 
 public class MenuBarController {
+
+    @FXML private Menu serverMenu;
 
     public void handleConnectClick(ActionEvent actionEvent) {
         try {
@@ -13,5 +18,6 @@ public class MenuBarController {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        serverMenu.getStyleClass().add("alive");
     }
 }
