584e715e4a79922fae40e93cac402b3e9cb7d311
Henrik Nilsson <henni142@student.liu.se>
2019-12-13 15:46:34 +0100

    Update MenuBarController to listen for updates to aliveStatus, updating the Server-menu buttons highlighting. Remove obsolete exception handling from handle click mehtod.

--- src/gui/controllers/MenuBarController.java
+++ src/gui/controllers/MenuBarController.java
@@ -4,9 +4,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 
+import remote.Car;
 import remote.Server;
-
-import java.io.IOException;
 
 /**
  * Menu Bar Controller class. This is the main controller of the Menu Bar.
@@ -16,17 +15,25 @@ public class MenuBarController {
     @FXML private Menu serverMenu;
 
     /**
-     * Handle click in the Server Connect-button. Tries to connect the server.
+     * Initialize method called by JavaFX thread when JavaFX-objects are constructed and ready.
+     * This method sets up a listener for alive status.
+     */
+    public void initialize() {
+        Car.getInstance().aliveStatus.subscribe(o -> {
+            if (o.booleanValue()) {
+                serverMenu.getStyleClass().add("alive");
+            }
+            else {
+                serverMenu.getStyleClass().remove("alive");
+            }
+        });
+    }
+
+    /**
+     * Handle click in the Server Connect-button. Calls server connect.
      * @param actionEvent Triggering event
      */
     public void handleConnectClick(ActionEvent actionEvent) {
-        try {
-            Server.getInstance().connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        // TODO Change alive status to a RemoteData of Car then implemented
-        serverMenu.getStyleClass().add("alive");
+        Server.getInstance().connect();
     }
 }
