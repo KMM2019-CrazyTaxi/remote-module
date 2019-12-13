77e311b0fc4536fe0996cf44e2043f48805aa025
Henrik Nilsson <henni142@student.liu.se>
2019-12-13 16:18:17 +0100

    Add alerts on server exception and error responses.

--- src/CCPanel.java
+++ src/CCPanel.java
@@ -1,8 +1,10 @@
+import enums.PacketType;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import remote.Server;
 
 public class CCPanel extends Application {
     public static void main(String[] args) {
@@ -14,5 +16,23 @@ public class CCPanel extends Application {
         root.getScene().addEventFilter(KeyEvent.KEY_PRESSED, root.getScene().lookup("#manualControlModule").getOnKeyPressed());
         root.getScene().addEventFilter(KeyEvent.KEY_RELEASED, root.getScene().lookup("#manualControlModule").getOnKeyReleased());
         root.show();
+
+        initialize();
+    }
+
+    private void initialize() {
+        // Show Error alert on server exception
+        Server.getInstance().addExceptionListener(o -> {
+            Alert a = new Alert(Alert.AlertType.ERROR, "Server exception:\n" + o.getMessage());
+            a.show();
+        });
+
+        // Show warning alert on server error
+        Server.getInstance().addResponsListener(o -> {
+            if (o.getCommand().getType() == PacketType.ERROR){
+                Alert a = new Alert(Alert.AlertType.WARNING, "Server error:\n" + o.getCommand());
+                a.show();
+            }
+        });
     }
 }
